67f8036cda8fd7b8dcdbf36b4a048c84a81bbd96
SF-LAP-874\rikesh <rikesh.b@simformsolutions.com>
2023-10-06 19:14:10 +0530

    Intial commit

---
+++ CopyFromOneContainerToAnother/AzureUtility.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFromOneContainerToAnother
+{
+    public class AzureUtility
+    {
+        public async Task UploadImage(BlobContainerClient containerClient, string blobName, string localImagePath)
+        {
+            // Get a reference to the blob
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            // Open the image file for reading
+            using (FileStream fs = File.OpenRead(localImagePath))
+            {
+                // Upload the image to the blob
+                await blobClient.UploadAsync(fs, true);
+            }
+        }
+
+        public async Task DownloadImage(BlobContainerClient containerClient, string blobName, string destinationPath)
+        {
+            // Get a reference to the blob
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            // Check if the blob exists
+            if (await blobClient.ExistsAsync())
+            {
+                // Download the blob to the specified destination
+                BlobDownloadInfo blobDownloadInfo = await blobClient.DownloadAsync();
+
+                // Open the destination file for writing
+                using (FileStream fs = File.OpenWrite(destinationPath))
+                {
+                    // Copy the blob content to the file
+                    await blobDownloadInfo.Content.CopyToAsync(fs);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Blob does not exist.");
+            }
+        }
+        public async Task CopyFromContainer(BlobClient sourceBlobClient, BlobContainerClient destinationContainerClient, string blobName)
+        {
+            if (await sourceBlobClient.ExistsAsync())
+            {
+                // Get a reference to the destination blob
+                BlobClient destinationBlobClient = destinationContainerClient.GetBlobClient(blobName);
+
+                // Start the blob copy operation
+                var copyInfo = await destinationBlobClient.StartCopyFromUriAsync(sourceBlobClient.Uri);
+
+                // Monitor the copy operation if needed
+                Console.WriteLine($"Copying blob: {copyInfo.HasCompleted}");
+            }
+            else
+            {
+                Console.WriteLine("Source blob does not exist.");
+            }
+        }
+    }
+
+}
---
+++ CopyFromOneContainerToAnother/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using CopyFromOneContainerToAnother;
+using Grpc.Core;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Reflection;
+
+Console.WriteLine("Demo for Azure Upload and Download and Copy blob from one container to another!");
+
+string sourceConnectionString = "DefaultEndpointsProtocol=https;AccountName=rikeshtestingpoc;AccountKey=8ciQvT4xn461vS+hmvrl59J5p4u7UX+zGUZXdvZpZQiHLTxwGH6g8dKUy7VMBemAoyoNxTmypQin+AStfkUgaA==;EndpointSuffix=core.windows.net";
+string destinationConnectionString = sourceConnectionString;
+
+string sourceContainerName = "testingpoc1";
+string destinationContainerName = "testingpoc2";
+string blobName = "file_example.jpg";
+
+// Create BlobServiceClient instances using the connection strings
+BlobServiceClient sourceServiceClient = new BlobServiceClient(sourceConnectionString);
+BlobServiceClient destinationServiceClient = new BlobServiceClient(destinationConnectionString);
+
+// Get a reference to the source blob container
+BlobContainerClient sourceContainerClient = sourceServiceClient.GetBlobContainerClient(sourceContainerName);
+
+// Get a reference to the destination blob container
+BlobContainerClient destinationContainerClient = destinationServiceClient.GetBlobContainerClient(destinationContainerName);
+
+// Get a reference to the source blob
+BlobClient sourceBlobClient = sourceContainerClient.GetBlobClient(blobName);
+
+// create an object of Azure utility class.
+AzureUtility azureUtility = new AzureUtility();
+
+await azureUtility.CopyFromContainer(sourceBlobClient, destinationContainerClient, blobName);
+
+Console.WriteLine("---------------------------------Uploading and downloading of file----------------------------------");
+
+string blobNameForUpload = "XYZ";
+// Create a BlobServiceClient using the connection string
+BlobServiceClient serviceClient = new BlobServiceClient(sourceConnectionString);
+
+// Get a reference to the container
+BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(sourceContainerName);
+
+// Path to the local image file for upload and download
+string localImagePath = "C:/Users/rikesh/Downloads/illustration6.svg";
+
+Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+string currDirPath =  Path.GetDirectoryName(Environment.CurrentDirectory.Split(new String[] { "bin" }, StringSplitOptions.None)[0]);
+
+string downloadDirPath = Path.Combine(currDirPath + "\\Downloads\\XYZ"); 
+
+AzureUtility azureUtility1 = new AzureUtility();
+await azureUtility1.UploadImage(containerClient, blobNameForUpload, localImagePath);
+await azureUtility1.DownloadImage(containerClient, blobNameForUpload, downloadDirPath);
+
